e210bbbe40af40e933134971ce9ab9eb248a9bba
b3c3 <b3c3@cecureintel.com>
2023-10-25 11:22:29 +0100

    Added more mapped CIDR blocks. Moved variable values from default block into a terraform.tfvars file.

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,14 @@ output "cac-c5-vpc-id" {
   description = "The VPC ID for CIL Academy C5"
   value       = data.aws_vpc.cac-c5-vpc-id.id
 }
+
+output "cbm-security-group-id" {
+  description = "The ID of the CIDR Block Mapped Security Group"
+  value       = aws_security_group.allow_cidr_blocks_sg.id
+}
+
+
+output "cbm-security-group-name" {
+  description = "The Name of the CIDR Block Mapped Security Group"
+  value       = aws_security_group.allow_cidr_blocks_sg.name
+}
--- variables.tf
+++ variables.tf
@@ -6,56 +6,7 @@ variable "cidr_block_map" {
   })))
 
   description = "Map of CIDR blocks and their associated descriptions and ports"
-
-  default = {
-    "10.18.24.0/22" = [
-      {
-        description = "Allow access from BOI VDI Pool M037-W10-X Offshore towards Algo AWA"
-        port        = 32020
-      },
-      {
-        description = "Allow access from BOI VDI Pool M037-W10-X Offshore towards Algo Stadium"
-        port        = 32040
-      },
-      {
-        description = "Allow access from BOI VDI Pool M037-W10-X Offshore towards Algo RiskWatch"
-        port        = 32050
-      },
-      {
-        description = "Allow access from BOI VDI Pool M037-W10-X Offshore towards Algo Apps"
-        port        = 32080
-      },
-      {
-        description = "Allow access from BOI VDI Pool M037-W10-X Offshore towards Algo RiskManager"
-        port        = 32090
-      }
-    ],
-
-    "10.20.24.0/22" = [
-      {
-        description = "Allow access from BOI VDI Pool M037-W10-X Offshore towards Algo RiskManager"
-        port        = 32090
-      },
-      {
-        description = "Allow access from BOI VDI Pool M037-W10-X Offshore towards Algo Apps"
-        port        = 32080
-      },
-      {
-        description = "Allow access from BOI VDI Pool M037-W10-X Offshore towards Algo RiskWatch"
-        port        = 32050
-      },
-      {
-        description = "Allow access from BOI VDI Pool M037-W10-X Offshore towards Algo Stadium"
-        port        = 32040
-      },
-      {
-        description = "Allow access from BOI VDI Pool M037-W10-X Offshore towards Algo AWA"
-        port        = 32020
-      }
-    ]
-  }
-
-  # Add more CIDR blocks with their associated rules
+  default     = {}
 }
 
 
