d0db8f447589eb86e1091b92094cb2ab2e142616
b3c3 <b3c3@cecureintel.com>
2023-10-25 10:50:57 +0100

    Change Dynamic block to reflect the attribute of security group - ingress

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "aws_security_group" "allow_cidr_blocks_sg" {
 
   // Other security group configuration settings
 
-  dynamic "sg_ingress_rules" {
+  dynamic "ingress" {
     for_each = flatten([
       for cbm_key, cbm_values in var.cidr_block_map : [
         for current_pdo_list_index, port_desc_object in cbm_values : {
@@ -18,11 +18,11 @@ resource "aws_security_group" "allow_cidr_blocks_sg" {
     ])
 
     content {
-      from_port   = sg_ingress_rules.value.port
-      to_port     = sg_ingress_rules.value.port
+      from_port   = ingress.value.port
+      to_port     = ingress.value.port
       protocol    = "tcp"
-      cidr_blocks = [sg_ingress_rules.value.inbound_cidr_block]
-      description = sg_ingress_rules.value.description
+      cidr_blocks = [ingress.value.inbound_cidr_block]
+      description = ingress.value.description
     }
   }
 }
